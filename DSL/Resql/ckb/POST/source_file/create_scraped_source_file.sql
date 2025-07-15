a6ddcdbf97d1aafd783aeb401e8e0483cef464a7
ahmer-mt <ahmer.ali@mindtitan.com>
2025-07-15 22:17:33 +0500

    ISSUE-20: remove source file ruuter DSL

--- DSL/Resql/ckb/POST/source_file/create_scraped_source_file.sql
+++ DSL/Resql/ckb/POST/source_file/create_scraped_source_file.sql
@@ -1,8 +1,8 @@
 INSERT INTO source_file (
     source_base_id, url, page_title, original_data_url, last_scraped_at, 
-    originally_scraped, type, status
+    originally_scraped, type
 )
 VALUES (
-    :source_base_id::UUID, :url, :page_title, :original_data_url, :scraped_at::TIMESTAMP WITH TIME ZONE, :scraped_at::TIMESTAMP WITH TIME ZONE, 'scraped_file'::source_file_type, 'finished'
+    :source_base_id::UUID, :url, :page_title, :original_data_url, :scraped_at::TIMESTAMP WITH TIME ZONE, :scraped_at::TIMESTAMP WITH TIME ZONE, 'scraped_file'::source_file_type
 )
 RETURNING id, base_id, source_base_id, url, page_title, original_data_url, last_scraped_at;
